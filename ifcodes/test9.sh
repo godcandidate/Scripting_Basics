4bcb08ff8aec4b189f4a5f60350cd5d12bbec7b6
godcandidate <godcandidate@gmail.com>
2023-03-08 19:34:24 +0000

    update test9.sh

--- ifcodes/test9.sh
+++ ifcodes/test9.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # testing if you can read a file
-pwfile=combina.py
-#
+
+# random file
+pwfile=test1.sh
+
 # first, test if the file exists, and is a file
 if [ -f $pwfile ]
 then
- # now test if you can read it
- if [ -r $pwfile ]
+	# now test if you can read it
+	if [ -r $pwfile ]
 	then
- 	tail  $pwfile
- else
- 	echo "Sorry, I am unable to read the $pwfile file"
- fi
+		echo "Reading $pwfile"
+		echo "_________________________"
+ 		tail  $pwfile
+		echo "_________________________"
+	else
+ 		echo "Sorry, I am unable to read the $pwfile file"
+	fi
 else
 	 echo "Sorry, the file $file does not exist"
 fi
