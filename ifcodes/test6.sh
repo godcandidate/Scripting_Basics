4a374d7a762a26f8a60daac3639e918ff686c951
godcandidate <godcandidate@gmail.com>
2023-03-08 19:06:21 +0000

    update test6.sh

--- ifcodes/test6.sh
+++ ifcodes/test6.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #comparing files
-#
+
+#random directory
 newdir=/sdcard/Eddy
-#
+
 if [ -d $newdir ]
 then
-	echo "newdir directory exists "
+	echo "$newdir directory exists "
 	cd $newdi
-	ls -l
+	ls
 else
-	echo "newdir directory does not exist"
+	echo "$newdir directory does not exist"
 fi
