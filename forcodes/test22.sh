f1e65d73afdc963bab0f35fb08a02871d3cb708d
godcandidate <godcandidate@gmail.com>
2023-04-02 11:22:37 +0000

    remove test2*

--- forcodes/test22.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#until loop
-#
-var=20
-until (( var == 1 ))
-do
-	var=$[ $var / 2 ]
-	echo " $var "
-done 
--- forcodes/test25.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
- # redirecting the for output to a file
- for (( a = 1; a < 10; a++ ))
- do
- echo "-- $a -- "
- done > ffile.txt
- echo "The command is finished."
